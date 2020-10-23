3b3a16b267d2a74a00d6500aa6a15b4fc2d97156
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-10-22 23:23:35 -0700

    added wow.js to about me

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import myPicture from '../img/mypicture.jpeg';
+import WOW from "wowjs";
 //import Card from 'react-bootstrap/Card';
 
 
 class About extends React.Component {
-
+    componentDidMount() {
+        const wow = new WOW.WOW();
+        wow.init();
+      }
 	render(){
         const styles= 
         {
@@ -23,10 +27,13 @@ class About extends React.Component {
 <div style={styles.wrapper}>
 
       
-  <div class="row no-gutters bg-light">
+  <div class="row no-gutters bg-light" style={{backgroundColor:'green'}}>
   <div class="col-md-5 mb-md-0 p-md-4">
-       
+  
+   <div className="wow bounceIn">
+      
    <h1>About Me</h1>
+</div>
     <img
 			className={"img-fluid " + this.props.fadeIn}
 			src={myPicture}
@@ -36,7 +43,26 @@ class About extends React.Component {
         </div>
     <div class="col-md-7 position-static p-4 pl-md-4">
     <h5 class="text-dark" ><h2>I'm</h2> <strong> Kanyarut Pornamnuay</strong></h5>
+    <h5>
+  I graduated with a master degree in computer science, and also joined the database management program and received a certificate from UCLA Extension. 
+</h5>
 
+<p>
+Moreover, I just graduated with a certificate program in full stack development from University of California, 
+Los Angeles coding bootcamp, with developed skills, I learn how to create websites and application
+using HTML/HTML5,CSS, Responsive web design, Bootstrap, Material-UI, ReactJS, JavaScript, jQuery, JSON, AJAX, APIs, NodeJS, ExpressJS,
+MySQL, MongoDB, Firebase and Github.
+  
+</p>
+
+<p>
+Known as an innovative problem solver passionate about developing apps, 
+with a focus on developing websites and applications. With a group projects,
+I have worked on a team to contributed to design and development of client and server database applications.
+Also, Maintain websites and applications, including content updates, debugging, testing
+and regular upgrades to ensure web and system usability. I’m excited to leverage my skills as part of a fast-paced, 
+quality-driven team to build better experiences on the web. 
+</p>
 
 
      </div>
@@ -48,18 +74,9 @@ class About extends React.Component {
 
 
 
-
-
-
-    
 </div>
 
 
-
-
-
-
-            
     
             );
         }
