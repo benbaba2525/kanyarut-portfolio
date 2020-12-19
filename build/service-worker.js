6caafbeb9f67fe06b73316c77bd3f1397d36549c
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-12-18 23:25:56 -0800

    Updated resume

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f4c892b7b2e013a455d0ed4ed17cc37e.js"
+  "/precache-manifest.fd02a89911111e54d7fff20f03de45ca.js"
 );
 
 self.addEventListener('message', (event) => {
